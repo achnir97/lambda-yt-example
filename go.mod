01c868831abedb09640de06f2b6d7d1f419f0d46
Nirmal Acharya <nirmalacharya@Nirmalui-iMac.local>
2023-01-07 11:41:50 +0900

    added all the codes

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/achnir97/go-lamda-function-3
+
+go 1.18
+
+require github.com/aws/aws-lambda-go v1.36.1
---
+++ main.go
@@ -0,0 +1,20 @@
+package main 
+import (
+	"fmt"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+type MyEvent struct{
+	Name string `json:"what  is your name?"`
+	Age int `json:"How old are you"`
+}
+
+type MyResponse struct {
+	Message string `json:"Answer:"`
+}
+func HandlLamdaEvent(event MyEvent) (MyResponse, error){
+	return MyResponse{Message:fmt.Sprintf("/%s is %s years old!", event.Name, event.Age)}, nil 
+}
+
+func main () {
+	 lamda.Start(HandlLamdaEvent)
+}
